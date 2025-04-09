99c12b85568a265ab06750d47752c4757dd71f08
Miguel Gerardo Falcon Rubio <MGFALCON@recal.local>
2025-04-09 11:33:42 -0600

    cambio test 4

--- perfilCliente.php
+++ perfilCliente.php
@@ -27,7 +27,7 @@ Todos los derechos reservados
 <div class="container">
 <div class="row">
 <div class="page-header ">
-<div> TEST update 1</div>
+<div> TEST update 2</div>
     <h1> Mi perfil  <small class="tittles-pages-logo">Bienvenido.</small></h1>
 </div>
 <?php
